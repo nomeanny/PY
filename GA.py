5d643f86c484be764a198415f9671498117d4471
Anny Beatriz <160553862+nomeanny@users.noreply.github.com>
2024-11-11 16:48:20 -0300

    Add files via upload

---
+++ GA.py
@@ -0,0 +1,8 @@
+# Gravação de números pares e ímpares em arquivos diferentes
+with open("impares.txt", "w") as impares:
+    with open("pares.txt", "w") as pares:
+        for n in range(0, 1000):
+            if n % 2 == 0:
+                pares.write(f"{n}\n")
+            else:
+                impares.write(f"{n}\n")
